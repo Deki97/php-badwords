4fc116845b09bad396605df1fd0ca7ac17f5f69e
Deki97 <dejan97@libero.it>
2022-01-21 17:04:12 +0100

    added comments and titles

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
         $testo = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla purus neque, elit in nunc sed, fermentum elit metus. Quisque elit augue nec tellus pellentesque, in elit leo cursus.';
     ?>
 
+    <h2>Paragrafo e lunghezza originali</h2>
     <!-- Stampo a schermo il paragrafo -->
     <p>
         <?php echo $testo; ?>
@@ -36,6 +37,7 @@
         ?>
     </p>
 
+    <h2>Paragrafo e lunghezza modificati</h2>
     <!-- Sostituisco tutte le occorrenze della parola da censurare con '***' -->
     <!-- Stampo il nuovo paragrafo  -->
     <p>
